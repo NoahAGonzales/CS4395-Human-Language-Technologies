beb6c0e01ff90ff2445d74fe0090769eeca6932d
Noah Gonzales <noahagonzo@yahoo.com>
2023-04-14 23:56:18 -0500

    temp

--- Homework9/Homework9_nag180001.py
+++ Homework9/Homework9_nag180001.py
@@ -2,40 +2,57 @@
 import nltk
 from nltk import word_tokenize
 nltk.download('stopwords')
-from nltk.corpus import stopwords
 STOPWORDS = stopwords.words('english')
+from sklearn.feature_extraction.text import CountVectorizer
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
+import random
+import string
 
 
-def preprocess_sentence(sent):    
-  # Tokenize sentence
-  words = nltk.word_tokenize(sent)
-  # Remove stop words
-  words = [w for w in words if w not in STOPWORDS]
-  # Lemmatize words
-  lemmatizer = WordNetLemmatizer()
-  words = [lemmatizer.lemmatize(w) for w in words]
-  return ' '.join(words)
+def preprocess_text(text):    
+  # Tokenize the text into individual words
+  tokens = word_tokenize(text.lower())
+  # Remove stopwords and punctuation
+  stop_words = set(stopwords.words('english') + list(string.punctuation)) # TODO: try removing stopwords and keeping punctionation
+  tokens1 = [token for token in tokens if token not in stop_words] 
+  # Return the filtered tokens as a string
+  return ' '.join(tokens1)
 
-def classify(sen):
-  sentence = preprocess_sentence(sen)
-  score = {}
-  for c in classes:
-      score[c] = 0
-  # Calculate score for each output class
-  for word in nltk.word_tokenize(sentence):
-      if word in corpus_words:
-          for c in classes:
-              score[c] += (1 + class_words[word][c]) / (len(classes) + corpus_words[word])
-  # Return the class with highest score
-  return max(score, key=score.get)
+# Define a function to generate a chatbot response
+def generate_response(user_input):
+  # Preprocess and tokenize the user input
+  preprocessed_input = preprocess_text(user_input)
+  input_vector = vectorizer.transform([preprocessed_input])
+  # Use the classifier to predict a response
+  predicted_category = clf.predict(input_vector)[0]
+  # Choose a random movie review from the predicted category
+  reviews_in_category = movie_reviews.fileids(predicted_category)
+  review_id = random.choice(reviews_in_category)
+  review_text = movie_reviews.raw(review_id)
+  # Return the review text as the chatbot response
+  return review_text
 
 
-# Define the interface
-while True:
-  # Get user input
-  sentence = input("You: ")
-  # Get chatbot response
-  response = classify(sentence)
-  # Print chatbot response
-  print("Chatbot: " + response)
+def main():
+  # import text
+  f = open("data/guts/0.txt" + "r", encoding="utf-8")
+  unprocessed_text = f.read()
+
+  # Extract Features
+  vectorizer = CountVectorizer()
+  corpus = [unprocessed_text]
+  vectorizer.fit_transform([preprocess_text(text) for text in corpus])
+
+
+  # temp
+  # Train ML model
+  corpus = [(preprocess_text(movie_reviews.raw(fileid)), category)
+          for category in movie_reviews.categories()]
+
+
+  return
+  
+if __name__=="__main__":
+  main()
